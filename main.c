538f35a60327395a87d8637f2a7c90c10e453ad9
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-08-04 00:49:38 -0700

    Use of vTaskSuspend and vTaskResume.
    vTaskSuspend(taskHandlerOfTheTaskYouWantToSuspend)
    vTaskResume(taskHandlerOfTheTaskYouWantToSuspend)

--- main.c
+++ main.c
@@ -8,12 +8,14 @@
 #include "string.h"
 
 
+
 TaskHandle_t task1Handler = NULL;
+TaskHandle_t task2Handler = NULL;
 
 void task1(void *p){
 
     int counter = 0;
-    // P is typecasted as a pointer to an integer -> (int *)p, the int[(int*)p] type-casts the pointer to a data variable of type 'int', and the resulting value is assigned to the integer variable messageTypeRetrieving
+    // P is type-casted as a pointer to an integer -> (int *)p, the int[(int*)p] type-casts the pointer to a data variable of type 'int', and the resulting value is assigned to the integer variable messageTypeRetrieving
     int messageTypeRetriving = (int)(int *)p;  // The void pointer is type-casted to ensure it is pointing to data type of intended data// Arrays are immutable - once defined, cannot be changed without re-initialization.
 
     while(1){
@@ -38,6 +40,38 @@ void task1(void *p){
     }
 }
 
+
+void task2(void *p){
+
+    TickType_t myLastTickCount;
+    myLastTickCount = xTaskGetTickCount(); // Get current tick count
+
+    uint8 message[] ="Task 2 has suspended Task 1. \r\n";
+    uint8 message1[] = "After suspending task 1, 1 Second Delay starting now.\r\n";
+    uint8 message2[] ="Resuming Task 1 now.\r\n";
+
+    while(1){
+            // Create 2 Second Delay before suspending Task 1 -> This will allow Task 1 to run for atleast 2 seconds.
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(2000));
+            // Suspend Task 1  using vTaskSuspend(taskHandler)
+            vTaskSuspend(task1Handler);
+
+            sciSend(scilinREG, (uint32_t)sizeof(message), &message[0]);
+
+           // Create a delay of 1 Seconds after Suspending Task 1.
+           sciSend(scilinREG, (uint32_t)sizeof(message1), &message1[0]);
+           vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(1000));
+
+           // Resuming Task 1 now.
+           sciSend(scilinREG, (uint32_t)sizeof(message2), &message2[0]);
+           vTaskResume(task1Handler);
+
+           // Allow Task 1 to run for 2 seconds
+           vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(2000));
+
+    }
+}
+
 int main(void)
 {
     // Arrays are immutable - once defined, cannot be changed without re-initialization.
@@ -47,17 +81,23 @@ int main(void)
     uint8 message [] = "Inside Main.\r\n";
     sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
 
-    // Passing arguments to a task;
+    // Creating two Tasks
 
+    xTaskCreate(task1,
+                "task1",
+                200,
+                (void *)messageType,
+                tskIDLE_PRIORITY,
+                &task1Handler);
 
-        xTaskCreate(task1,
-                    "task1",
+    xTaskCreate(task2,
+                    "task2",
                     200,
-                    (void *)messageType,
+                    (void *)0,
                     tskIDLE_PRIORITY,
-                    &task1Handler);
+                    &task2Handler);
 
-        vTaskStartScheduler();
+    vTaskStartScheduler();
 
 	while(1);
 }
