cb815fe00a62b61804318fe270b4fa23f6fc3a36
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-08-04 02:01:58 -0700

    Implementing Interrupt Handling with FreeRTOS Framework.
    Software Interrupt emulation by calling ISR inside a task.
    
    Suspending a task at start which is resumed by the interrupt source's
    ISR.
    
    Currently, code stuck at line 50, where interruptHandling task is being
    resumed. Suspecting that we need hardware interrupts not software
    emulated interrupts.

--- main.c
+++ main.c
@@ -9,18 +9,64 @@
 
 
 TaskHandle_t task1Handler = NULL;
+TaskHandle_t interruptHandler = NULL;
+TaskHandle_t task3Handler = NULL;
+
+volatile int counter = 0;
 
 void task1(void *p){
 
 
     while(1){
 
-            uint8 message [] = "Task 1 is executing every 0.5 second.\r\n";
+            uint8 message [] = "Task 1 is executing every 1 second.\r\n";
             sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-            vTaskDelay(500);
+            vTaskDelay(1000);
+            counter+=10; // Increment counter by  10 after delay.
 
     }
 }
+
+void interruptRoutineTask(void *p){
+    uint8 message [] = "Interrupt Handling Task is suspended.\r\n";
+    uint8 message1 [] = "Interrupt Handling Task is resumed.\r\n";
+    while(1){
+        /*
+         * The following line will suspend the task itself.
+         * This task will suspend itself after this line and won't execute anything after until it is resumed by the ISR handler.
+         */
+        sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
+        vTaskSuspend(NULL);
+        sciSend(scilinREG, (uint32_t) sizeof(message1), &message1[0]);
+        counter = 0; // Refresh the counter.
+
+    }
+}
+void ISR_Handler(){
+
+    uint8 message [] = "ISR Handler is resuming the task now.\r\n";
+    sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
+    BaseType_t checkIfYieldRequired;
+    checkIfYieldRequired = xTaskResumeFromISR(interruptHandler);
+    portYIELD_FROM_ISR(checkIfYieldRequired);
+
+}
+
+void emulatingInterrupt(void *p){
+
+    TickType_t lastTickCount;
+    lastTickCount = xTaskGetTickCount();
+
+    uint8 message [] = "CPU Interrupted.\r\n";
+    while(1){
+
+        vTaskDelayUntil(&lastTickCount, pdMS_TO_TICKS(5000));  // Create a Delay of 5 seconds, this will block this task for 5 seconds, allowing task 1 to run.
+        sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
+        ISR_Handler();  // Call the ISR handler
+    }
+}
+
+
 int main(void)
 {
     sciInit();
@@ -29,10 +75,24 @@ int main(void)
     sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
         xTaskCreate(task1,
                     "task1",
-                    200,
+                    150,
                     (void *)0,
                     tskIDLE_PRIORITY,
                     &task1Handler);
+
+        xTaskCreate(interruptRoutineTask,
+                    "interruptRoutineTask",
+                    150,
+                    (void *)0,
+                    tskIDLE_PRIORITY+1,  // Higher priority than the previous task. Getting Garbage output on terminal when both at same priority.
+                    &interruptHandler);
+
+        xTaskCreate(emulatingInterrupt,
+                            "EmulatingAnInterrupt",
+                            150,
+                            (void *)0,
+                            tskIDLE_PRIORITY,  // Higher priority than the previous task. Getting Garbage output on terminal when both at same priority.
+                            &task3Handler);
 
         vTaskStartScheduler();
 
