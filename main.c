ff5397652a9aa270b267992de5926df9a639a64a
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-08-03 20:55:37 -0700

    Implemented different RTOS task delay functions.
    Looked at vTaskDelay.
    Calculating Tick Time to get the desired delay in Millisecond.
    Looked at vTaskDelayUntil to get a more accurate delay.

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include "os_task.h"
 #include "sci.h"
 #include "string.h"
+#include "FreeRTOSConfig.h"
+#include "os_projdefs.h"
 
 
 TaskHandle_t task1Handler = NULL;
@@ -13,18 +15,33 @@ TaskHandle_t task1Handler = NULL;
 void task1(void *p){
 
 
+    TickType_t myLastTickCount;
+    myLastTickCount = xTaskGetTickCount();
     while(1){
-
-            uint8 message [] = "Task 1 is executing every 0.5 second.\r\n";
+            uint8 message [] = "Task 1 is executing every 1 second.\r\n";
             sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-            vTaskDelay(500);
+            //vTaskDelay(1000); // Getting a delay
+
+            /*  For 1 second delay, use the kernel tick time defined in FreeRTOSConfig.h */
+            //vTaskDelay(1000 * configTICK_RATE_HZ /1000); // This is more precise then the previous Delay, but not most accurate delay of 1 second.
+
+            /*  Instead of writing the equation above -> "1000 * configTICK_RATE_HZ /1000" to get the number of ticks
+             *  corresponding to your desired delay in millisecond, you can use a function called pdMS_TO_TICKS(millisecond), returns
+             *     Tick Time*/
+            //vTaskDelay(pdMS_TO_TICKS(1000));  // This gives a delay of "alteast" 1 second, but is slightly more than a 1 second -> Explained in FreeRTOS doc
+
+
+            /* To get a more accurate delay of 1 Second - use vTaskDelayUntil(currentTickTime, DesiredMsInTickTime) */
+            vTaskDelayUntil(&myLastTickCount,pdMS_TO_TICKS(1000)); // This will give you exactl 1 second block Time for this task.
+
+            /* Anything after the previous statement will only be executed once the Delay is completed. */
+
 
     }
 }
 int main(void)
 {
     sciInit();
-
     uint8 message [] = "Inside Main.\r\n";
     sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
         xTaskCreate(task1,
@@ -33,8 +50,7 @@ int main(void)
                     (void *)0,
                     tskIDLE_PRIORITY,
                     &task1Handler);
-
-        vTaskStartScheduler();
+    vTaskStartScheduler();
 
 	while(1);
 }
