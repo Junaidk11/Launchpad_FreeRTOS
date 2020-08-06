4e1f48f28b2728acc18ef482fc7d1a0626399820
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-08-05 21:31:51 -0700

    A basic main function to make use of the 32-bit notification value.
    Using a variation of notification sending to a task, where we set
    desired bits of the 32-bit notification value.
    The receiving task will look at the desired bits and if they're set, it
    will perform a print to the serial terminal.
    
    Still need to test the different actions after sending notification from
    a task. But this idea could be used for setting flags that could be used
    by a different task to do something in a certain condition.

--- main.c
+++ main.c
@@ -14,62 +14,51 @@ TaskHandle_t task2Handler = NULL;
 
 void task1(void *p){
 
-     TickType_t myLastTickCount;
-
-     myLastTickCount = xTaskGetTickCount();
-    // Send notification from task1 to task2
+    TickType_t myLastTickCount;
+    myLastTickCount = xTaskGetTickCount();
+    /*
+     * Looking at a variation of Sending Notification from one task to another.
+     * In this setup, we set the desired bits of the 32-bit unsigned notification value and send them./
+     * The different bits of the 32-bit notification value can be used as flags by the receiving task.
+     */
 
 
     while(1){
 
-            /*
-             *  The following function will send a 32-bit value to task2. The 32-bit value by default is '0'.
-             *  When this function is called, the 32-bit value is incremented by 1 and sent to task2
-             */
-
-            // Send notification 1
-          xTaskNotifyGive(task2Handler);
-           // Send notification 2
-          xTaskNotifyGive(task2Handler);
-           // Send notification 3
-          xTaskNotifyGive(task2Handler);
-           // Send notification 4
-          xTaskNotifyGive(task2Handler);
-
-          // Create a 5 second delay
-          vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(5000));
+            xTaskNotify(task2Handler, (1<<2) | (1<<1) |(1<<0),eSetBits);  //Set bit 0, 1, 2
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(1000)); // 1 Second delay
 
 
+
+            xTaskNotify(task2Handler, (1<<3),eSetValueWithOverwrite);  //Set bit 3 of Notification value, and then increment the Notification value by 1.
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(1000)); // 1 Second delay
 
     }
 }
 
 void task2(void *p){
 
-
-    int notificationValue;
-    char messageBuffer[30];
+    char messageBuffer[15];
+    uint32_t ulNotificationValue; // To store the received notification value
     while(1){
 
-        /*
-         *  The following function will receive a 32-bit value from task1. The 32-bit value by default is '0'.
-         *  When this function is called, the second argument is used to tell FreeRTOS kernel to either decrement the 32-bit value received  by 1 or reset it.
-         *      uint32_t value returned  <---  ulTaskNotifyTake(DecrementOrReset, BlockingTime);
-         *
-         *      if first argument = 0 -> the 32-bit value received from task 1 is decremented by 1
-         *      if first argument = 1 -> the 32-bit value received from task 1 is Reset after reading.
-         *
-         */
-        notificationValue = ulTaskNotifyTake(pdFALSE, (TickType_t)portMAX_DELAY);
+        if(xTaskNotifyWait(((1<<2) | (1<<1) |(1<<0)), 0, &ulNotificationValue, portMAX_DELAY)== pdTRUE){
 
+            // Notification value received, cleared bits 0,1,2, not clearing bit 3
+            if (ulNotificationValue & (7<<0)){  // Checking if all 3 bits were set in the notification
+                sprintf(messageBuffer, "Green.\r\n");
+                sciSend(scilinREG,(uint32_t)sizeof(messageBuffer),(uint8 *)messageBuffer);
+            }/*else if(ulNotificationValue & (1<<3)){  // Checking if bit 3 of the Notification value is set
+                sprintf(messageBuffer, "Blue.\r\n");
+                sciSend(scilinREG,(uint32_t)sizeof(messageBuffer),(uint8 *)messageBuffer);
+            }*/
 
-        if(notificationValue > 0){
-            sprintf(messageBuffer, "Notification Received: %d. \r\n", notificationValue);
-            sciSend(scilinREG, (uint32_t)sizeof(messageBuffer), (uint8 *)messageBuffer);
+            if(ulNotificationValue & (1<<3)){  // Checking if bit 3 of the Notification value is set
+                sprintf(messageBuffer, "Blue.\r\n");
+                sciSend(scilinREG,(uint32_t)sizeof(messageBuffer),(uint8 *)messageBuffer);
+            }
+
         }
-
-
-
     }
 }
 
