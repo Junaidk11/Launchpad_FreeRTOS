3d1c25389b582942ca9164f68b0ad0a7f1d4e203
Junaid Jawed Khan <jjkhan@sfu.ca>
2021-03-23 23:03:06 -0700

    Removed debug folder.

--- Debug/source/errata_SSWF021_45.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-source/errata_SSWF021_45.obj: ../source/errata_SSWF021_45.c
-source/errata_SSWF021_45.obj: /Users/junaidkhan/Documents/Launchpad_FreeRTOS/include/errata_SSWF021_45_defs.h
-source/errata_SSWF021_45.obj: /Users/junaidkhan/Documents/Launchpad_FreeRTOS/include/errata_SSWF021_45.h
-
-../source/errata_SSWF021_45.c: 
-/Users/junaidkhan/Documents/Launchpad_FreeRTOS/include/errata_SSWF021_45_defs.h: 
-/Users/junaidkhan/Documents/Launchpad_FreeRTOS/include/errata_SSWF021_45.h: 
