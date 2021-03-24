5ae5fa22d43c16b780ad92c14813d51cfce56de8
Junaid Jawed Khan <jjkhan@sfu.ca>
2021-03-23 22:16:21 -0700

    Moved interrupt handling main to its own folder.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
- * main.c : This main function is the test code written to implement interrupt handling in FreeRTOS
+ * main.c
  */
 
 #include "FreeRTOS.h"
@@ -9,73 +9,66 @@
 
 
 TaskHandle_t task1Handler = NULL;
-TaskHandle_t interruptHandler = NULL;
-TaskHandle_t task3Handler = NULL;
+TaskHandle_t task2Handler = NULL;
 
-volatile int counter = 0;
 
 void task1(void *p){
 
-
+    TickType_t myLastTickCount;
+    myLastTickCount = xTaskGetTickCount();
     while(1){
 
-            uint8 message [] = "Task 1 is executing every 1 second.\r\n";
+            uint8 message [] = "Task 1 is executing every 2 second.\r\n";
             sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-            vTaskDelay(1000);
-            counter+=10; // Increment counter by  10 after delay.
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(2000)); // 2 Second Delay
 
     }
 }
 
-void interruptRoutineTask(void *p){
-    uint8 message [] = "Interrupt Handling Task is suspended.\r\n";
-    uint8 message1 [] = "Interrupt Handling Task is resumed.\r\n";
-    while(1){
-        /*
-         * The following line will suspend the task itself.
-         * This task will suspend itself after this line and won't execute anything after until it is resumed by the ISR handler.
-         */
-        sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-        vTaskSuspend(NULL);
-        sciSend(scilinREG, (uint32_t) sizeof(message1), &message1[0]);
-        counter = 0; // Refresh the counter.
 
-    }
-}
-void ISR_Handler(){
-
-    uint8 message [] = "ISR Handler is resuming the task now.\r\n";
-    sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-    vTaskResume(interruptHandler);
-
-    // Yield?
-    //vPortYield();   // Not sure if we need to Yield here because this function is not a task.
+void task2(void *p){
 
 
-    /*
-     *  The Following Lines are used if hardware interrupt used. In this example we're emulating a software interrupt.
-     */
-    //BaseType_t checkIfYieldRequired;
-    //checkIfYieldRequired = xTaskResumeFromISR(interruptHandler);
-    //portYIELD_FROM_ISR(checkIfYieldRequired);
+    char myTaskList[300];  // A buffer to hold the information about all tasks running on the kernel.
+    TickType_t myLastTickCount;
+    myLastTickCount = xTaskGetTickCount();
 
 
-
-}
-
-void emulatingInterrupt(void *p){
-
-    TickType_t lastTickCount;
-    lastTickCount = xTaskGetTickCount();
-
-    uint8 message [] = "CPU Interrupted.\r\n";
+    uint8 message2 [] ="State of Task 1:\r\n";
+    uint8 message3 [] ="State of Task 2:\r\n";
+    uint8 message4 [] ="Displaying information on all tasks running on the kernel.\r\n";
+    //eTaskState taskStates;
+    //uint8 taskState[]="0";
     while(1){
 
-        vTaskDelayUntil(&lastTickCount, pdMS_TO_TICKS(5000));  // Create a Delay of 5 seconds, this will block this task for 5 seconds, allowing task 1 to run.
-        sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-        ISR_Handler();  // Call the ISR handler
+            uint8 message [] = "Task 2 is executing every 1 second.\r\n";
+            sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(1000)); // 1 Second Delay
+
+            //taskStates = eTaskGetState(task1Handler); // Get state of task1
+            //taskState = taskStates;
+            sciSend(scilinREG, (uint32_t) sizeof(message2), &message2[0]);
+            //sciSend(scilinREG, (uint32_t) sizeof(taskState), &taskState);
+
+
+
+            //taskStates = eTaskGetState(task1Handler); // Get state of task2
+            //taskStateHolder[0] = (uint8)taskStates;
+
+
+            sciSend(scilinREG, (uint32_t) sizeof(message3), &message3[0]);
+            //sciSend(scilinREG, (uint32_t) sizeof(taskStates), &taskStates);
+
+            vTaskDelayUntil(&myLastTickCount, pdMS_TO_TICKS(1000)); // 1 Second Delay - this will allow Task 1 to run
+
+            vTaskList(myTaskList); // This function will store the task information of all the tasks running of the freeRTOS kernel.
+            sciSend(scilinREG, (uint32_t) sizeof(message4), &message4[0]);
+            sciSend(scilinREG, (uint32_t) sizeof((uint8)myTaskList), &myTaskList[0]);
+
     }
+
 }
+
 
 
 int main(void)
@@ -84,27 +77,20 @@ int main(void)
 
     uint8 message [] = "Inside Main.\r\n";
     sciSend(scilinREG, (uint32_t) sizeof(message), &message[0]);
-        xTaskCreate(task1,
-                    "task1",
-                    150,
-                    (void *)0,
-                    tskIDLE_PRIORITY,
-                    &task1Handler);
 
-        xTaskCreate(interruptRoutineTask,
-                    "interruptRoutineTask",
-                    150,
-                    (void *)0,
-                    tskIDLE_PRIORITY+1,  // Higher priority than the previous task. Getting Garbage output on terminal when both at same priority.
-                    &interruptHandler);
+    xTaskCreate(task1,
+                "task1",
+                200,
+                (void *)0,
+                tskIDLE_PRIORITY,
+                &task1Handler);
 
-        xTaskCreate(emulatingInterrupt,
-                            "EmulatingAnInterrupt",
-                            150,
-                            (void *)0,
-                            tskIDLE_PRIORITY,
-                            &task3Handler);
-
+    xTaskCreate(task2,
+                "task2",
+                200,
+                (void *)0,
+                tskIDLE_PRIORITY+1,
+                &task2Handler);
         vTaskStartScheduler();
 
 	while(1);
